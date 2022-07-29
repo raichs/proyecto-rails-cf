851a66970df893d325d10015cff319a2d4731cb0
jbances <jbances@digital.gob.pe>
2022-07-29 01:25:28 -0500

    eliminando comentarios

--- app/mailers/new_article_mailer.rb
+++ app/mailers/new_article_mailer.rb
@@ -1,19 +1,7 @@
 class NewArticleMailer < ApplicationMailer
 
   def send_mail_new_product(article)
-
-    # @article = params[:article]
-
-    # mail(to: "jcbances6@gmail.com", subject: "Producto agregado!")
-    # mail(to: user.email, subject: "Resultado de exportación de #{text}")
-
     @article = article
     mail(to: "jcbances6@gmail.com", subject: "Producto agregado!")
-    
-    
-    
-
-
-
   end
 end
